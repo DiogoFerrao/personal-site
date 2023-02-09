55ab41c7e5c43f4328c0982e63dd038f6055f3a0
Diogo Ferrão <diogoferrao@tecnico.ulisboa.pt>
2023-02-09 14:48:00 +0000

    Format files, update teaching assistant

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Diogo Ferrão';
-export const SITE_DESCRIPTION = 'My small corner of the internet.';
+export const SITE_TITLE = "Diogo Ferrão";
+export const SITE_DESCRIPTION = "My small corner of the internet.";
